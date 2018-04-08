51a5d4906160066d009dfa5df7874247634f4860
Om Prakash Singh <omps@omps.in>
2018-04-08 15:08:45 +0530

    Sandbox for the work i am doing

---
+++ strava_api/strava.js
@@ -0,0 +1,23 @@
+var StravaApiV3 = require('strava_api_v3');
+var defaultClient = StravaApiV3.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: strava_oauth
+var strava_oauth = defaultClient.authentications['strava_oauth'];
+strava_oauth.accessToken = "YOUR ACCESS TOKEN"
+
+var api = new StravaApiV3.ActivitiesApi()
+
+var id = 789; // {Long} The identifier of the activity.
+
+var opts = { 
+  'includeAllEfforts': true // {Boolean} To include all segments efforts.
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+api.getActivityById(id, opts, callback);
